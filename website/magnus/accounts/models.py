f52841da22f7662d611126dd6c7e909e27e39050
jithendra1798 <siddaartha1798@gmail.com>
2021-10-19 12:26:51 +0530

    Add Registration Backend

--- website/magnus/accounts/models.py
+++ website/magnus/accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Register(models.Model):
+    user_full_name = models.CharField(max_length = 100)
+    user_email = models.TextField()
+    user_password = models.TextField()
+    user_gender = models.CharField()
+    user_address = models.TextField()
+    user_phone = models.IntegerField()
+    user_dob = models.DateField()
+    user_aadhaar = models.IntegerField()
+    user_aadhar_photo  = models.ImageField(upload_to = 'user_aadhaar_photo')
+    user_photo  = models.ImageField(upload_to = 'user_photos')
--- website/magnus/magnus/settings.py
+++ website/magnus/magnus/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -80,8 +81,11 @@ WSGI_APPLICATION = 'magnus.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'magnus',
+        'USER': 'postgres',
+        'PASSWORD': '12345678',
+        'HOST': 'localhost'
     }
 }
 
